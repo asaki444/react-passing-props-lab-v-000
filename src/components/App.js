5deba687aee53b5b0d25899481b169478c569169
Aditi Green <asaki444@gmail.com>
2017-11-13 23:46:06 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
-
+import React, { Component }  from 'react';
 import FruitBasket from './FruitBasket';
+import { getFruitBasket } from '../fruit'
 
-const App = () => <FruitBasket />;
+// const App = () => <FruitBasket />;
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filters: [],
+      fruit: [],
+      currentFilter: null
+    };
+  }
+  render(){
+      return <FruitBasket filters={getFruitBasket}/>
+  }
+}
 
 export default App;
